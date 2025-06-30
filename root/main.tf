a63546be421100a219532ce035703d30d2aa291b
ks-ahmed <ka-sharif@outlook.com>
2025-06-30 20:51:29 +0100

    modularize root

--- root/main.tf
+++ root/main.tf
@@ -1,6 +1,4 @@
 
-
-# --- VPC MODULE ---
 module "vpc" {
   source          = "./modules/vpc"
   vpc_cidr        = var.vpc_cidr
@@ -8,14 +6,12 @@ module "vpc" {
   private_subnets = var.private_subnets
 }
 
-# --- KEY PAIR MODULE ---
 module "key_pair" {
   source                  = "./modules/key_pair"
   key_name                = var.key_name
   private_key_output_path = var.private_key_output_path
 }
 
-# --- RDS MODULE ---
 module "rds" {
   source     = "./modules/rds"
   vpc_id     = module.vpc.vpc_id
@@ -25,7 +21,6 @@ module "rds" {
   db_user    = var.db_user
 }
 
-# --- EC2 MODULE (WordPress) ---
 module "ec2" {
   source             = "./modules/ec2"
   subnet_id          = module.vpc.public_subnet_ids[0]
